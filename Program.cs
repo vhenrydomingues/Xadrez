f75fd7b3d5c92bed18e9cadffe4a40a8ef9dc631
HENRY <vhenrydomingues@gmail.com>
2022-04-12 09:09:49 -0300

    Ajustes na rotina, criação de classe PartidaDeXadrez, e ajustes na implementação da posição

--- Program.cs
+++ Program.cs
@@ -2,13 +2,19 @@
 using xadrez;
 using Xadrez;
 
-Tabuleiro tab = new Tabuleiro(8,8);
+try
+{
+    PartidaDeXadrez partida = new PartidaDeXadrez();
 
+ 
 
-tab.colocarPeca(new Torre(tab,Cor.Preta) , new Posicao(0,0));
-tab.colocarPeca(new Rei(tab,Cor.Preta) , new Posicao(1,3));
-tab.colocarPeca(new Torre(tab,Cor.Preta) , new Posicao(2,4));
+    Tela.imprimirTabuleiro(partida.tab);
 
-Tela.imprimirTabuleiro(tab);
+}
+catch (TabuleiroException e)
+{
+
+   Console.WriteLine(e.Message);
+}
 
 Console.ReadLine();
